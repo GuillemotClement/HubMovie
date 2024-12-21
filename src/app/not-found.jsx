e8b19e016e6bea515f47a95ffa94e173091e3eae
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-21 12:21:59 +0100

    Add error handler

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ export default function PageNotFound() {
   return (
       <div className="grid place-content-center min-h-[calc(100vh-theme(spacing.32))]">
         <div className="text-center">
-          <h1>Erreur</h1>
+          <h1>404</h1>
           <p>La page demandée n'existe pas encore</p>
         </div>
       </div>
